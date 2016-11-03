c3b798aba62fa68cf4b6b31ba964c1752f4415df
“Suada <suada.haji@andela.com>
2016-11-03 12:46:46 +0300

    Modify the AppModule class

--- app/src/main/java/com/android/java/miss/dependencyinjectionwithdagger2/dagger/AppModule.java
+++ app/src/main/java/com/android/java/miss/dependencyinjectionwithdagger2/dagger/AppModule.java
@@ -1,9 +1,29 @@
 package com.android.java.miss.dependencyinjectionwithdagger2.dagger;
 
+import android.content.Context;
+
+import com.android.java.miss.dependencyinjectionwithdagger2.ProntoShopApplication;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
 /**
  * This is a standard Java class that will be decorated with Dagger 2 specific annotations
  * and it is the methods in this class that provide the dependencies.
  */
 
+@Module
 public class AppModule {
+  private final ProntoShopApplication app;
+
+  public AppModule(ProntoShopApplication app) {
+    this.app = app;
+  }
+
+  @Provides @Singleton
+  public Context provideContext() {
+    return app;
+  }
 }
